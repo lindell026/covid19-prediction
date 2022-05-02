be13e2b67f995546018ce50aba47f3348c426b01
lindell026 <45280391+lindell026@users.noreply.github.com>
2022-05-02 18:22:03 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=13smVsOiuNLo8H8QLJXy-nKg_IbleHNRG'
+export_file_name = 'pickle_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['positive', 'negative']
 path = Path(__file__).parent
 
 app = Starlette()
