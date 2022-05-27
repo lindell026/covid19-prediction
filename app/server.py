a4e67f6138c8e923563c324cb773a1371f56a6c9
lindell026 <45280391+lindell026@users.noreply.github.com>
2022-05-27 17:41:53 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -64,27 +64,6 @@ async def analyze(request):
     prediction = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
-@app.route("/", methods=['GET'])
-def hello():
-    return "hey"
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    lr = joblib.load("model.pkl")
-    if lr:
-        try:
-            json = request.get_json()  
-            model_columns = joblib.load("model_cols.pkl")
-            temp=list(json[0].values())
-            vals=np.array(temp)
-            prediction = lr.predict(temp)
-            print("here:",prediction)        
-            return jsonify({'prediction': str(prediction[0])})
-
-    except:        
-        return jsonify({'trace': traceback.format_exc()})
-    else:
-        return ('No model here to use')
 
 
 if __name__ == '__main__':
